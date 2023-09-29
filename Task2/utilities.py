1d1586a432b515d4defd30ace624621265aa5525
robinzyb <38876805+robinzyb@users.noreply.github.com>
2023-09-29 23:34:29 +0200

    update Task2 Part II

--- Task2/utilities.py
+++ Task2/utilities.py
@@ -43,3 +43,26 @@ def plot_data(
     if plot_filename:
         fig.savefig(plot_filename)
     fig.show()
+
+def plot_data2(
+        results: npt.ArrayLike,
+        xlabel: str='r',
+        plot_filename: ty.Union[None, str] = None
+        
+):
+    plt.style.use("../utils/plotstyle.mplstyle")
+    row = 1
+    col = 1
+    fig = plt.figure(figsize=(3.37*col, 1.89*row), dpi=300,facecolor='white')
+    gs = fig.add_gridspec(row,col)
+    ax  = fig.add_subplot(gs[0])
+    for key, val in results.items():
+        if key == xlabel:
+            continue
+        ax.plot(results[xlabel], val, label=key)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel('quantity')
+    ax.legend()
+    if plot_filename:
+        fig.savefig(plot_filename)
+    fig.show()
